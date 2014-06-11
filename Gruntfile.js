3248def3283b80a93250059c687dabf44d45d955
Golo Roden <webmaster@goloroden.de>
2014-06-12 01:08:41 +0200

    Introduced tourism.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,12 @@
 'use strict';
 
-module.exports = function (grunt) {
-  grunt.initConfig({
-    jshint: {
-      files: [ '**/*.js', '!node_modules/**/*.js', '!public/**/*.js' ],
-      options: {
-        jshintrc: 'jshint.json'
-      }
-    },
+var tourism = require('tourism');
 
-    mochaTest: {
-      test: {
-        options: {
-          bail: true,
-          reporter: 'spec',
-          ui: 'tdd'
-        },
-        src: ['test/**/*.js']
-      }
-    },
-
-    watch: {
-      scripts: {
-        files: [ '**/*.js', '!node_modules/**/*.js' ],
-        tasks: [ 'default' ],
-        options: {
-          interrupt: true
-        }
-      }
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-mocha-test');
-
-  grunt.registerTask('default', [ 'jshint', 'mochaTest' ]);
-};
+module.exports = tourism({
+  analyse: {
+    server: [ '**/*.js', '!node_modules/**/*.js' ]
+  },
+  test: {
+    server: [ 'test/**/*.js' ]
+  }
+});
