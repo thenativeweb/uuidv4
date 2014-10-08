352a18c279d0ac00471a75da4981d3408cb93c3e
Golo Roden <golo.roden@thenativeweb.io>
2014-10-08 17:28:08 +0200

    Modernized code.

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # uuid
 
-uuid creates UUIDs.
+uuid creates v4 UUIDs.
 
 ## Installation
 
-At the moment, installation of this module must be made manually.
+    $ npm install uuidv4
 
 ## Quick start
 
-The first thing you need to do is to integrate uuid into your project by using the `require` function.
+The first thing you need to do is to integrate uuidv4 into your project by using the `require` function.
 
 ```javascript
-var uuid = require('uuid');
+var uuid = require('uuidv4');
 ```
 
 Then you can create UUIDs. To do so simply call the `uuid` function.
@@ -30,7 +30,7 @@ console.log(uuid.empty());
 
 ## Running the build
 
-This module can be built using [Grunt](http://gruntjs.com/). Besides running the tests, this also analyses the code. To run Grunt, go to the folder where you have installed uuid and run `grunt`. You need to have [grunt-cli](https://github.com/gruntjs/grunt-cli) installed.
+This module can be built using [Grunt](http://gruntjs.com/). Besides running the tests, this also analyses the code. To run Grunt, go to the folder where you have installed uuidv4 and run `grunt`. You need to have [grunt-cli](https://github.com/gruntjs/grunt-cli) installed.
 
     $ grunt
 
--- test/uuidTests.js
+++ test/uuidTests.js
@@ -2,28 +2,31 @@
 
 var assert = require('node-assertthat');
 
-var uuid = require('../lib/uuid');
+var uuidv4 = require('../lib/uuidv4');
 
-suite('uuid', function () {
-  test('returns a UUID.', function () {
+suite('uuidv4', function () {
+  test('returns a v4 UUID.', function (done) {
     var uuidRegex = /[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}/;
 
-    var actual = uuid();
+    var actual = uuidv4();
 
     assert.that(actual, is.ofType('string'));
     assert.that(uuidRegex.test(actual), is.true());
+    done();
   });
 
-  test('returns a different UUID on each call.', function () {
-    var actualFirst = uuid(),
-        actualSecond = uuid();
+  test('returns a different v4 UUID on each call.', function (done) {
+    var actualFirst = uuidv4(),
+        actualSecond = uuidv4();
 
     assert.that(actualFirst, is.not.equalTo(actualSecond));
+    done();
   });
 
   suite('empty', function () {
-    test('returns the 0000... UUID.', function () {
-      assert.that(uuid.empty(), is.equalTo('00000000-0000-0000-0000-000000000000'));
+    test('returns 00000000-0000-0000-0000-000000000000.', function (done) {
+      assert.that(uuidv4.empty(), is.equalTo('00000000-0000-0000-0000-000000000000'));
+      done();
     });
   });
 });
