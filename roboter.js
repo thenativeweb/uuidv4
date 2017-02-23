d775f8821556afd3d92a0d40b7c25350a2d71841
Golo Roden <golo.roden@thenativeweb.io>
2017-02-23 10:53:10 +0100

    Exclude dist directory from code analysis.

--- roboter.js
+++ roboter.js
@@ -6,7 +6,7 @@ roboter.
   workOn('server').
   equipWith(task => {
     task('universal/analyze', {
-      src: [ '**/*.js', '!node_modules/**/*.js', '!coverage/**/*.js' ]
+      src: [ '**/*.js', '!node_modules/**/*.js', '!coverage/**/*.js', '!dist/**/*.js' ]
     });
 
     task('universal/release', {
