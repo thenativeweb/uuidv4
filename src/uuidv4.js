d1d47baecfae5aaa65ec5edbabbd722446401cf8
Sven Eppler <sven.eppler@sodgeit.de>
2018-09-18 11:21:07 +0200

    Return false on invalid uuids
    Instead of throwing an exception i'd like to get false back.

--- src/uuidv4.js
+++ src/uuidv4.js
@@ -11,7 +11,7 @@ uuidv4.regex = /^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-
 
 uuidv4.is = function (value) {
   if (!value) {
-    throw new Error('Value is missing.');
+    return false;
   }
 
   return uuidv4.regex.test(value);
